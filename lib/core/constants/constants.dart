858f88ad3af06cf3825508d62b1b063753fd1a67
quyettc1112 <TranQuyet02588x1112002@gmail.com>
2025-04-07 12:06:57 +0700

    Setup Config KeyCloak App Auth

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,7 @@
+const AUTH_DOMAIN = "auth.minity.tech";
+const AUTH_CLIENT_ID = "flutter-client";
+const AUTH_ISSUER = "https://keycloak.unwind.id.vn/realms/flutter-test-realm";
+const BUNDLE_IDENTIFIER = "com.example.bruh";
+const AUTH_REDIRECT_URI = "com.example.bruh:/login-callback";
+const AUTH_REFRESH_TOKEN_KEY = 'refresh-token';
+const AUTH_REALMS = "drilling-today";
